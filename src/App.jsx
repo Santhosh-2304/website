53f85ac135887e6d6be4096e325c83b817cb6184
Santhosh-2304 <lionelsanthosh2004@gmail.com>
2025-10-28 16:27:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Header from './components/Header'
 import Block1 from './components/Block1'
 import Block2 from './components/Block2'
 import Block3 from './components/Block3'
-
+import Footer from './components/Footer'
 
 function App() {
-  
-
   return (
     <>
      <Header />
      <Block1 />
      <Block2 />
      <Block3 />
+     <Footer />
     </>
   )
 }
