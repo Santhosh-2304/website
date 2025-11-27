9a65cf0365f9768ee2b9e019f90d2ce3ab178602
Santhosh-2304 <lionelsanthosh2004@gmail.com>
2025-11-27 09:26:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SoftwareTraining from './pages/SoftwareTraining'
+import SoftwareTech from './pages/SoftwareTech'
+import AboutUs from './pages/AboutUs'
+import Contact from './pages/Contact'
+import Careers from './pages/Careers'
+import AcademicTraining from './pages/AcademicTraining'
+import TrainingTalent from './pages/TrainingTalent'
+import CorporateTraining from './pages/CorporateTraining'
+import Research from './pages/Research'
 import Header from './components/Header'
-import Block1 from './components/Block1'
-import Block2 from './components/Block2'
-import Block3 from './components/Block3'
 
-
-function App() {
+const App = () => {
   return (
     <>
-     <Header />
-     <Block1 />
-     <Block2 />
-     <Block3 />
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/software-training-services' element={<SoftwareTraining />} />
+      <Route path='training-talent' element={<TrainingTalent />} />
+      <Route path='/academic-training' element={<AcademicTraining />} />
+      <Route path='/corporate-training' element={<CorporateTraining />} />
+      <Route path='/software-technology-solutions' element={<SoftwareTech />} />
+      <Route path='/research-development' element={<Research />} />
+      <Route path ='/careers' element={<Careers />} />
+      <Route path='/aboutus' element={<AboutUs />} />
+      <Route path ='/contact' element={<Contact />} />
+    </Routes>
     </>
   )
 }
