caf4fdc17ad2835bded2f459343e13ab2a2888a6
Santhosh-2304 <lionelsanthosh2004@gmail.com>
2025-11-27 20:17:41 +0530

    Update SoftwareTechnology.jsx

--- src/pages/SoftwareTechnology.jsx
+++ src/pages/SoftwareTechnology.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import TechBlock from '../components/TechBlock'
+import Block1 from '../components/Block1'
 const SoftwareTechnology = () => {
   return (
     <>
+      <div className='h-[500px]'>
+      </div>
       <TechBlock />
     </>
   )
