849b876e27c13d64cb4e5432829e9625aa04ebef
Santhosh-2304 <lionelsanthosh2004@gmail.com>
2025-11-27 20:16:31 +0530

    Update Block2.jsx

--- src/components/Block2.jsx
+++ src/components/Block2.jsx
@@ -1,26 +1,23 @@
-import React from 'react'
+import React from "react";
+import InfoBlockText from "../components/InfoBlockText";
 
-function Block2() {
+export default function Block2() {
   return (
-    <div className='block2'>
-        <div className='block2-0'>
-        <div className='block2-1'>
-            <h5>Become an upskilling master</h5>
-            <p>
-                Close skills gaps and keep your tech talent thanks to thousands of on-demand, expert-led courses and the ability to learn at your preferred pace. And while you can’t upskill in starship ops (yet), you can master skills in just-as-popular topics, such as cloud, security, Python, Linux, DevOps, AWS, and security.
-            </p>
-        </div>
-         <img src='src\assets\cloud-practioner.webp'  style={{borderRadius:"25px",width:"500px",height:"300px",margin:"20px 40px"}}></img>
-         </div>
-        <div className='block2-0'>
-             <img src='src\assets\next-level-learning.webp'  style={{borderRadius:"25px",width:"500px",height:"300px",margin:"20px 40px"}}></img>
-        <div className='block2-1'>
-            <h5>Combine courses with labs for next-level learning </h5>
-            <p>Pair in-progress skills with a secure, provisioned practice environment to improve project readiness, speed to mastery, and that all-important peace of mind for learners and leaders. You’ll also boost confidence in your team’s ability to deliver on key initiatives.</p>
-        </div>
-        </div>
-    </div>
-  )
-}
+    <div className="block2">
 
-export default Block2
+      <InfoBlockText
+        title="Become an upskilling master"
+        text="Close skills gaps and keep your tech talent thanks to thousands of on-demand, expert-led courses and the ability to learn at your preferred pace..."
+        image="src/assets/cloud-practioner.webp"
+      />
+
+      <InfoBlockText
+        title="Combine courses with labs for next-level learning"
+        text="Pair in-progress skills with a secure, provisioned practice environment to improve project readiness and speed to mastery..."
+        image="src/assets/next-level-learning.webp"
+        reverse
+      />
+
+    </div>
+  );
+}
