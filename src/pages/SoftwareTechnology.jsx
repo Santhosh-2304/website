05ba2bd4343114e178b3596ac5fb82cae4b4bd41
Santhosh-2304 <lionelsanthosh2004@gmail.com>
2025-11-28 16:01:01 +0530

    Update SoftwareTechnology.jsx

--- src/pages/SoftwareTechnology.jsx
+++ src/pages/SoftwareTechnology.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import TechBlock from '../components/TechBlock'
-import Block1 from '../components/Block1'
+import Footer from '../components/Footer'
+import TitleBlock from '../components/TitleBlock'
+
+
 const SoftwareTechnology = () => {
   return (
     <>
-      <div className='h-[500px]'>
-      </div>
+      <TitleBlock 
+      image="src\assets\SoftwareTech.jpg"
+      title="Software & Technology Solutions"
+      description="Empowering businesses through advanced software solutions and cutting-edge IT
+                  services. At IMMT, we blend expertise, innovation, and collaboration to propel your
+                  organization into the digital future. From cloud technologies to AI-driven platforms, our
+                  mission is your success."
+      />
       <TechBlock />
     </>
   )
