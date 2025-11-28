0be9c30536220aa458b7175f39f501354c11e0d3
Santhosh-2304 <lionelsanthosh2004@gmail.com>
2025-11-28 16:00:42 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,22 @@
-import Block1 from '../components/Block1'
 import Block2 from '../components/Block2'
 import Block3 from '../components/Block3'
 import Footer from '../components/Footer'
-
 import React from 'react'
+import ImageCarousel from '../components/ImageCarousel'
 
+const images = [
+      "src/assets/training.jpg",
+      "src/assets/Corporate.jpg"
+]
 const Home = () => {
   return (
     <>
-        <Block1 />
+        <ImageCarousel 
+          images={images}
+          interval={4000}
+        />
         <Block2 />
         <Block3 />
-        <Footer />
     </>
   )
 }
