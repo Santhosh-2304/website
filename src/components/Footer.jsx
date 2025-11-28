fd2e51185885e5579c96c1ec1d40471decba68a6
Santhosh-2304 <lionelsanthosh2004@gmail.com>
2025-11-28 15:54:10 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 function Footer() {
   return (
     <div className='footer'>
-        <div className='contact'>
-            <h6 style={{fontSize:"30px",margin:0}}>Contact Us</h6>
-            <p><i class="fa-solid fa-location-dot" /> Door No.3/1A, Plot No. 1 and 2, First Floor, Kalvi Nagar, Madurai</p>
-            <p>📞 9940642303</p>
-            <p><i class="fa-solid fa-envelope"></i> innovationalmettlemindstech@gmail.com</p>
+        <div className='contact '>
+            <h4 className='font-bold text-xl'>IMMT – Innovating Today. Empowering Tomorrow.</h4>
+            <p>© 2025 IMMT | All Rights Reserved.</p>
+            <p> Services: Software Development | Consulting | IoT | Embedded Systems | Training |
+            R&D | Digital Marketing</p>
+            Connect With Us: LinkedIn | Facebook | Instagram
         </div>
     </div>
   )
