d0d20581f43bcab1b826992d19c5b16ae39ff4cd
Santhosh-2304 <lionelsanthosh2004@gmail.com>
2025-11-28 16:01:19 +0530

    Update TrainingTalent.jsx

--- src/pages/TrainingTalent.jsx
+++ src/pages/TrainingTalent.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import InfoBlockText from '../components/InfoBlockText'
-
+import TitleBlock from '../components/TitleBlock'
 const TrainingTalent = () => {
   return (
     <div>
+      <TitleBlock 
+       image="src/assets/training.jpg"
+        title="Training Talent"
+        description="Our specialized training programs cover software technologies, digital tools, and
+                    emerging trends. With a practical learning approach, we prepare professionals to
+                    stay relevant and excel in dynamic IT environments.
+                    We connect organizations with skilled professionals and deliver end-to-end
+                    workforce solutions. Our resourcing services ensure the right talent is aligned with
+                    the right opportunity."
+      />
         <InfoBlockText
         title="Fresher training & Placement"
         text="At IMMT, we empower fresh graduates to kickstart their careers with confidence.
@@ -13,20 +23,20 @@ const TrainingTalent = () => {
             soft skill enhancement, and interview preparation.
             With dedicated mentorship and strong industry collaborations, we ensure that every
             participant becomes job-ready and equipped to excel in the world of IT."
-        image="src/assets/cloud-practioner.webp"
+        image="src/assets/cons.avif"
       />
 
       <InfoBlockText
-        title="Lateral/Experienced training &amp; Placement"
+        title="Lateral/Experienced training & Placement"
         text="For working professionals seeking to upgrade their skills or transition to advanced
-                roles, IMMT offers specialized Lateral/Experienced Training &amp; Placement
+                roles, IMMT offers specialized Lateral/Experienced Training & Placement
                 Programs.
                 Our expert-led modules focus on deep technical knowledge, practical scenarios,
                 architecture-level understanding, and leadership capabilities.
                 We help professionals grow in their careers through advanced upskilling, curated
                 learning paths, cross-functional training, and direct placement support with top
                 technology companies."
-        image="src/assets/next-level-learning.webp"
+        image="src/assets/web.jpg"
         reverse
       />
     </div>
