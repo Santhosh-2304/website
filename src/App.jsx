5962a1652e577b2f689a3101bdbb3793d50e5974
Santhosh-2304 <lionelsanthosh2004@gmail.com>
2025-12-01 17:46:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './pages/Home'
-import SoftwareTechnology from './pages/SoftwareTechnology'
+import Services from './pages/Services'
+import SoftwareTechnology from './pages/SoftwareTech'
 import TrainingTalent from './pages/TrainingTalent'
 import AcademicTraining from './pages/AcademicTraining'
 import CorporateTraining from './pages/CorporateTraining'
 import Careers from './pages/Careers'
-import About from './pages/About'
+import About from './pages/AboutUs'
 import Contact from './pages/Contact'
 import Footer from './components/Footer'
+import SoftwareTraining from './pages/SoftwareTraining'
 
 
 const App = () => {
@@ -18,6 +20,8 @@ const App = () => {
     <Header />
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/services' element={<Services />} />
+      <Route path='/software-training-services' element={<SoftwareTraining />} />
       <Route path='/training-talent' element={<TrainingTalent/>} />
       <Route path='/academic-training' element={<AcademicTraining />} />
       <Route path='/corporate-training' element={<CorporateTraining />} />
